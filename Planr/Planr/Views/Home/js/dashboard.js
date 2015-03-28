53354056b2dbddb1117b19ae1259390ba67df244
Olivier Brochu Dufour <olibd@hotmail.com>
2015-03-28 00:11:46 -0400

    big commit: refactored schedule.js canvas into a scheduleCanvas class; imported the course class; completed the schedule class; created a schedules class; removed the user class

--- Planr/Planr/Views/Home/js/dashboard.js
+++ Planr/Planr/Views/Home/js/dashboard.js
@@ -52,20 +52,6 @@ StudentRecord.prototype = {
 };
 
 /////////////////////////////////
-//User Class
-////////////////////////////////
-function User(url, recordDOMID){
-	var studentRecord;
-	var preferences;
-	var schedule;
-	var sequence;
-	this.recordUrl = url;
-	this.DOMID = recordDOMID;
-	this.self = this;
-		
-}
-
-/////////////////////////////////
 // Preferences class
 /////////////////////////////////
 function Preferences(url, preferencesDOMID){
@@ -253,20 +239,114 @@ Sequence.prototype = {
 	//TODO check if course placed in a summer sessions
 };
 
-
-
 /////////////////////////////////
 // Schedule class
 /////////////////////////////////
 
-function Schedule(coursesArray, scheduleDOMID){
+function Schedule(coursesArray, scheduleSemester, scheduleYear){
 	
 	// private instace variables, array of course object
 	var courses = coursesArray;
+	var semester = scheduleSemester;
+	var year = scheduleYear;
+
+	this.self = this;
 	
-	//create the schedule's canvas
-	var canvas = document.createElement('canvas'); //create canva element
-	canvas.id = 
+	// accessor methods
+	
+	this.getCoursesArray = function(){
+		return courses;
+	};
+	
+	this.setCoursesArray = function(coursesArray){
+		courses = coursesArray;
+	};
+	
+	this.getSemester = function(){
+		return semester;
+	};
+	
+	this.getYear = function(){
+		
+	};
+	
+}
+
+/////////////////////////////////
+// Schedules class
+/////////////////////////////////
+function Schedules(url, preferencesOBJ, year, ajaxCallback){
+	//public instance variables
+	//boolean stating if there is an ajax call to the server
+	this.fetching = false;
+	//ajax object
+	this.jqxhr = null;
+	this.schedulesUrl = url;
+	this.preferences = preferencesOBJ;
+	this.callback = ajaxCallback;
+	this.winterSchedules = null;
+	this.fallSchedules = null;
+	this.summerSchedules = null;
+	this.year = year;
+	this.self = this;
+}
+
+Schedules.prototype = {
+	constructor: Schedule,
+	
+	//fetch schedules from server
+	getSchedulesFromServer:function(){
+		if(!this.fetching){
+			this.fetching = true;
+			
+			var self = this.self;
+			
+			//fetch using get, set ajax ovject
+			this.jqxhr = $.get(this.schedulesUrl + '?morning=' + this.preferences.getMorning() + '&night=' + this.preferences.getNight() + '&dayoff' + this.preferences.getDayOff(), function(data){
+				//build the schedule options based on the received data
+				self.makeSchedules(data);
+				self.fetching = false;
+				//run a callback setted by the constructor
+				self.callback();
+			});
+			
+		}
+	},
+	
+	//make schedule object based on supplied data array, the array should have the format semesters[schedules[courses[]]]
+	makeSchedules:function(data){
+		var self = this.self;
+		//create fall schedules array
+		this.fallSchedules = [];
+		$.each(data[0], function(i, o){
+			//push new schedules to schedules array
+			self.fallSchedules.push(new Schedule(o[i], 'Fall', self.year));
+		});
+		
+		//create winter schedules array
+		this.winterSchedules = [];
+		$.each(data[1], function(i, o){
+			//push new schedules to schedules array
+			self.fallSchedules.push(new Schedule(o[i], 'Winter', self.year));
+		});
+		
+		//TODO: add check to make sure there is a summer semester given
+		//create summer schedules array
+		this.summerSchedules = [];
+		$.each(data[2], function(i, o){
+			//push new schedules to schedules array
+			self.fallSchedules.push(new Schedule(o[i], 'Summer', self.year));
+		});
+		
+	}
+};
+/////////////////////////////////
+// Canvas class
+/////////////////////////////////
+function ScheduleCanvas(canvasID) {
+	
+	//define varables
+	var canvas = document.getElementById(canvasID);
 	canvas.width = 1800;
 	canvas.height = 2880;
 	canvas.style.width = "600px";
@@ -274,30 +354,144 @@ function Schedule(coursesArray, scheduleDOMID){
 	var ctx = canvas.getContext("2d");
 	ctx.scale(3, 3);
 	var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-	var hour = ["08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "15:00", "15:15", "15:30", "15:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00"]
+	var hour = ["08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "15:00", "15:15", "15:30", "15:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00"];
 	var time = [];
 	var backColor = ["black", "gray", "gold", "cyan", "darkMagenta", "navy"];
 	var frontColor = ["yellow", "white", "mediumBlue", "blue", "Gainsboro", "white"];
 	
-	this.DomId = scheduleDOMID;
-	this.self = this;
-	
-	// accessor methods
-	
-	this.getCourses = function(){
-		return courses;
+	//some black magic here, TODO extend the array class
+	Array.prototype.min = function() {
+		return Math.min.apply(null, this);
+	};
+	Array.prototype.max = function() {
+		return Math.max.apply(null, this);
 	};
 	
-	this.setCourses = function(coursesArray){
-		courses = coursesArray;
-	};	
+	//The methods, TODO: refactor them in the prototype
+	
+	//function to draw one day of a course. 
+	this.draw = function(name, day, start, end, type, bcolor, fcolor) {
+		var xAxis = (days.indexOf(day) + 1) * 100;
+		var startHour = start.substr(0, 2).concat(":00");
+		var startY = (time.indexOf(startHour) * 15) + (parseInt(start.substr(3, 2)) * 1) + 15;
+		var endHour = end.substr(0, 2).concat(":00");
+		var endY = (time.indexOf(endHour) * 15) + parseInt(end.substr(3, 2)) + 15;
+		ctx.fillStyle = bcolor;
+		ctx.fillRect(xAxis, startY, 100, endY - startY);
+		ctx.font = '10pt Calibri';
+		ctx.textAlign = 'center';
+		ctx.fillStyle = fcolor;
+		ctx.fillText(name, xAxis + 50, (startY + endY) / 2);
+		ctx.fillText(type.concat(" ", start, "-", end), xAxis + 50, (startY + endY) / 2 + 15);
+	};
+	
+	//this method draw the schedule canvas to the dom element for the provided id
+	this.drawSchedule = function (schedule) {
+		var start = this.minStart(schedule);
+		var finish = this.maxFinish(schedule);
+		var s = 0;
+		for (var i = start; i <= finish; i++) {
+			time.push(hour[i]);
+		}
+		for (var x = 100; x < 600; x += 100) {
+			var day = days[s];
+			ctx.beginPath();
+			ctx.strokeStyle = "grey";
+			ctx.lineWidth = "0.5";
+			ctx.rect(x, 0, 100, time.length * 15);
+			ctx.stroke();
+			ctx.font = '10pt Calibri';
+			ctx.textAlign = 'center';
+			ctx.fillStyle = "red";
+			ctx.fillText(day, x + 50, 12);
+			s += 1;
+		}
+		s = 0;
+		for (var y = 15; y < time.length * 15; y += 15) {
+			var hours = time[s];
+			ctx.beginPath();
+			ctx.strokeStyle = "grey";
+			ctx.lineWidth = "0.5";
+			ctx.rect(0, y, 600, 15);
+			ctx.stroke();
+			ctx.font = '10pt Calibri';
+			ctx.textAlign = 'center';
+			ctx.fillStyle = "red";
+			ctx.fillText(hours, 50, y + 12);
+			s += 1;
+		}
+		for (var i = 0; i < schedule.length; i++) {
+			schedule[i].drawCourse(backColor[i], frontColor[i]);
+		}
+	};
+	
+	this.maxFinish = function(a) {
+		var b = [];
+		for (var i = 0; i < a.length; i++) {
+			var y1 = a[i].endL.substr(0, 2).concat(":00");
+			var y2 = a[i].endT.substr(0, 2).concat(":00");
+			var y3 = a[i].endLab.substr(0, 2).concat(":00");
+			if (hour.indexOf(y1) != -1) b.push(hour.indexOf(y1));
+			if (hour.indexOf(y2) != -1) b.push(hour.indexOf(y2));
+			if (hour.indexOf(y3) != -1) b.push(hour.indexOf(y3));
+		}
+		return b.max() + 4;
+	};
+	
+	this.minStart = function(a) {
+		var b = [];
+		for (var i = 0; i < a.length; i++) {
+			var y1 = a[i].startL.substr(0, 2).concat(":00");
+			var y2 = a[i].startT.substr(0, 2).concat(":00");
+			var y3 = a[i].startLab.substr(0, 2).concat(":00");
+			if (hour.indexOf(y1) != -1) b.push(hour.indexOf(y1));
+			if (hour.indexOf(y2) != -1) b.push(hour.indexOf(y2));
+			if (hour.indexOf(y3) != -1) b.push(hour.indexOf(y3));
+		}
+		return b.min();
+	};
+	
 	
 }
 
-Schedule.prototype = {
-	constructor:Schedule,
-	
-	
-	
-	
-};
+/////////////////////////////////
+// Course class
+/////////////////////////////////
+function Course(name, lDay1, lDay2, startL, endL, tDay1, tDay2, startT, endT, labDay1, labDay2, startLab, endLab) {
+	this.name = name;
+	this.lDay1 = lDay1;
+	this.lDay2 = lDay2;
+	this.startL = startL;
+	this.endL = endL;
+	this.tDay1 = tDay1;
+	this.tDay2 = tDay2;
+	this.startT = startT;
+	this.endT = endT;
+	this.labDay1 = labDay1;
+	this.labDay2 = labDay2;
+	this.startLab = startLab;
+	this.endLab = endLab;
+	this.drawCourse = function(bcolor, fcolor) {
+		if (name != "") {
+			if (lDay1 != "") {
+				draw(name, lDay1, startL, endL, "Lec", bcolor, fcolor);
+			}
+			if (lDay2 != "") {
+				draw(name, lDay2, startL, endL, "Lec", bcolor, fcolor);
+			}
+			if (tDay1 != "") {
+				draw(name, tDay1, startT, endT, "Tut", bcolor, fcolor);
+			}
+			if (tDay2 != "") {
+				draw(name, tDay2, startT, endT, "Tut", bcolor, fcolor);
+			}
+			if (labDay1 != "") {
+				draw(name, labDay1, startLab, endLab, "Lab", bcolor, fcolor);
+			}
+			if (labDay2 != "") {
+				draw(name, labDay2, startLab, endLab, "Lab", bcolor, fcolor);
+			}
+
+		}
+	};
+}
