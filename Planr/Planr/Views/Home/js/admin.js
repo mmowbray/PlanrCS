ee30fc5899b3ad58ebbb23897ffdddb66ce85d45
Firas Krayem <fkrayem@gmail.com>
2015-04-08 01:18:39 -0400

    live search box

--- Planr/Planr/Views/Home/js/admin.js
+++ Planr/Planr/Views/Home/js/admin.js
@@ -130,4 +130,12 @@ function CourseTable ()
 		document.getElementById(stringSemester[i]).checked=false;
 		}
 	}
-	
+	var $rows = $('#course_table tr');
+	$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+	});
