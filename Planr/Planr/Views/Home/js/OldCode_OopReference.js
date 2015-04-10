f4b1b5ad7447a4eae672d728c5c2b328f098173a
Chris <christopher.yazbek@gmail.com>
2015-04-10 13:15:15 -0400

    copy from oli to chris

---
+++ Planr/Planr/Views/Home/js/OldCode_OopReference.js
@@ -0,0 +1,19 @@
+/*
+function getStudentRecord () {
+		return record;
+}
+
+function setStudentRecord (recordObj) {
+	record = recordObj;
+}
+
+function StudentRecord(url, recordDOMID){
+	var record = null;
+	this.recordUrl = url;
+	this.DOMID = recordDOMID;
+	this.self = this;
+	this.getRecord = getStudentRecord;
+	this.setRecord = setStudentRecord;
+	
+}
+*/
---
+++ Planr/Planr/Views/Home/js/style.js
@@ -0,0 +1,39 @@
+/*$(".field_input").focus(function(){
+  $(this).parent().addClass("is-active is-completed");
+});
+
+$(".field_input").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("is-completed");
+  $(this).parent().removeClass("is-active");
+});
+*/
+
+
+jQuery(document).ready(function($){  
+	
+	
+$('[data-toggle="tooltip"]').tooltip()
+	
+	
+$("#un").focusin(function(){
+	$($(".field_label")[0]).css("display", "block");
+});
+
+$("#un").focusout(function(){
+	if($("#un").val().length === 0){
+		$($(".field_label")[0]).hide();
+	}
+});
+	
+$("#pass").focusin(function(){$($(".field_label")[1]).css("display", "block");});
+$("#pass").focusout(function(){
+		if($("#pass").val().length === 0){
+			$($(".field_label")[1]).hide();
+		}
+	});
+
+/*$("#day_time").click(function(){$("#day_time").toggleClass( "pref_icon_selected");});
+$("#night_time").click(function(){$("#night_time").toggleClass( "pref_icon_selected");});
+$("#day_off").click(function(){$("#day_off").toggleClass( "pref_icon_selected");});*/
+});
